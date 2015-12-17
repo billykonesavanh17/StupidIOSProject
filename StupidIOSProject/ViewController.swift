2abeb8b1ccf223125ebf0e8a386f9134afc22c99
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-12-17 08:37:14 -0700

    Added a color changing method and @IBAction

--- StupidIOSProject/ViewController.swift
+++ StupidIOSProject/ViewController.swift
@@ -11,7 +11,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func changeColor(sender: UIButton) {
+       
+        view.backgroundColor = UIColor.whiteColor()
+   
     }
+    
+    @IBAction func changeColorSwitch(sender: UISwitch) {
+        view.backgroundColor = UIColor.blackColor()
+    }
+    
+    @IBAction func changeColorSlider(sender: UISlider) {
+        view.backgroundColor = UIColor.magentaColor()
+    }
+    func randomColorChange()
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
